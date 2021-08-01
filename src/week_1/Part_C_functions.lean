c9a8b9fa491d7f363bea53a5d8cf543a8e737c7c
andrewparr <10243811+andrewparr@users.noreply.github.com>
2021-08-01 14:48:49 +0100

    some notes on part 1.c

--- src/week_1/Part_C_functions.lean
+++ src/week_1/Part_C_functions.lean
@@ -28,6 +28,7 @@ variables (a b x : X) (y : Y) (z : Z)
 -- let's start by checking the definition of injective is
 -- what we think it is.
 
+-- This definition is saying the function f maps distinct elements to distinct elements
 lemma injective_def : injective f ↔ ∀ a b : X, f a = f b → a = b :=
 begin
   -- true by definition
@@ -37,6 +38,9 @@ end
 -- You can now `rw injective_def` to change `injective f` into its definition.
 
 -- The identity function id : X → X is defined by id(x) = x. Let's check this
+
+-- Note. The defintion if id is
+-- def id {α : Sort u} (a : α) : α := a
 
 lemma id_def : id x = x :=
 begin
@@ -49,7 +53,18 @@ end
 /-- The identity function is injective -/
 lemma injective_id : injective (id : X → X) :=
 begin
-  sorry,
+  -- following the comments, lets change the statement that id is injective to its defintion
+  rw injective_def,
+  -- we now have ∀ (a, b), id a = id b → a = b
+  -- so we use intro to make an a and a b
+  intros a b,
+  -- use rw id_def twice to change the id a and then id b to a and b
+  rw id_def,
+  rw id_def,
+  --tautology, -- we have to prove a = b → a = b, which can be proved with tautology,
+  -- or by intro and exact as we haven't covered tautology yet.
+  intro h, 
+  exact h,
 end
 
 -- function composition g ∘ f is satisfies (g ∘ f) (x) = g(f(x)). This
@@ -66,7 +81,23 @@ lemma injective_comp (hf : injective f) (hg : injective g) : injective (g ∘ f)
 begin
   -- you could start with `rw injective_def at *` if you like.
   -- In some sense it doesn't do anything, but it might make you happier.
-  sorry,
+  rw injective_def at *, -- it makes it clearer.
+  intros a b h,
+  rw comp_def at h,
+  rw comp_def at h,
+  apply hf,
+  apply hg,
+  exact h,
+end
+
+example (hf : injective f) (hg : injective g) : injective (g ∘ f) :=
+begin
+  -- example to show you don't need the rw for definational statements
+  -- they just make it clearer when using interactive theorem prover.
+  intros a b h,
+  apply hf,
+  apply hg,
+  exact h,
 end
 
 /-!
@@ -87,7 +118,10 @@ end
 lemma surjective_id : surjective (id : X → X) :=
 begin
   -- you can start with `rw surjective_def` if you like.
-  sorry,
+  -- rw surjective_def,
+  intro a,
+  use a,
+  rw id_def,
 end
 
 -- If you started with `rw surjective_def` -- try deleting it.
@@ -99,8 +133,41 @@ end
 /-- Composite of two surjective functions is surjective -/
 lemma surjective_comp (hf : surjective f) (hg : surjective g) : surjective (g ∘ f) :=
 begin
-  sorry,
+  rw surjective_def at *, -- again not necessary but helps to see what to do.
+  intro z,
+  {
+    -- I can't seem to rw comp_def here, so just recall that (g ∘ f) x = g (f x)
+    -- therefore we need to prove that
+    --  ∃ (x : X), g (f x) = z
+    -- we have hg: ∀ (y : Z), ∃ (x : Y), g x = y and z : Z
+    -- The hint said the `have` tactic is helpful, but I couldn't get it to work
+    -- and the solution doesn't use it anyway.
+    -- we have a z and so hg z should give us a y.  But this is done with cases
+    cases hg z with y hy,
+    -- we now do this again, now with y to get an z using hf
+    cases hf y with x hx,
+    use x,
+    -- goal is now (g ∘ f) x = z, which is g (f x) = z
+    -- hx tells us f x = y
+    -- The followin shoe line is required.  But it isn't explained what it does.
+    -- before the goal is (g ∘ f) x = z, after it the goal is g (f x) = z
+    -- these are the same!
+    show g(f(x)) = z,
+    -- I don't know why I couldn't just have rw hx, rw hy without this show line!
+    rw hx,
+    rw hy,
+  },
 end
+
+/-
+  The documentation for show is:
+  `show t` finds the first goal whose target unifies with `t`.
+  It makes that the main goal, performs the unification,
+  and replaces the target with the unified version of `t`.
+  On page 19 of https://leanprover-community.github.io/mathematics_in_lean/mathematics_in_lean.pdf
+  It talks about the show tactic to structure the proof.  The text says the show is not needed but
+  it helps to structure the proof. Above however it seems to be needed.
+-/
 
 /-!
 
